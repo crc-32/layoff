8f8a417e19cd07644fc2344831ac43ec87664313
XorTroll <33005497+XorTroll@users.noreply.github.com>
2019-01-26 22:08:38 +0100

    Missing extern, I think

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 // Include the main libnx system header, for Switch development
 #include <switch.h>
 
-u32 __nx_applet_type = AppletType_OverlayApplet;
+extern u32 __nx_applet_type = AppletType_OverlayApplet;
 
 // Main program entrypoint
 int main(int argc, char* argv[])
