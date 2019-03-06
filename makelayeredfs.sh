02383791de5f2739433ec10b87437b447e2f5be3
jakibaki <jakibaki@live.com>
2019-03-06 09:07:36 +0100

    Use mkdir -p instead of multiple mkdir calls

--- makelayeredfs.sh
+++ makelayeredfs.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 if test -z "$1"
 then
-    mkdir atmosphere
-    mkdir atmosphere/titles
-    mkdir atmosphere/titles/010000000000100C
-    mkdir atmosphere/titles/010000000000100C/flags
+    mkdir -p atmosphere/titles/010000000000100C/flags
     touch atmosphere/titles/010000000000100C/flags/fsmitm.flag
     cp -R romfs atmosphere/titles/010000000000100C/
     cp layoff.nsp atmosphere/titles/010000000000100C/exefs.nsp
 else
-    mkdir $1/atmosphere
-    mkdir $1/atmosphere/titles
-    mkdir $1/atmosphere/titles/010000000000100C
+    mkdir -p $1/atmosphere/titles/010000000000100C
     cp layoff.nsp $1/atmosphere/titles/010000000000100C/exefs.nsp
 fi
